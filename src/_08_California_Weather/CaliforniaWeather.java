1c0f0ed5af490b4bf1abb73ddbc35b3921c48ad0
smellstink <54760319+OceanNut420@users.noreply.github.com>
2022-11-18 16:47:45 -0800

    Level 3 Module 01 08 complete!

--- src/_08_California_Weather/CaliforniaWeather.java
+++ src/_08_California_Weather/CaliforniaWeather.java
@@ -1,8 +1,13 @@
 package _08_California_Weather;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /*
  * OBJECTIVE:
@@ -29,11 +34,23 @@ import javax.swing.JOptionPane;
  * temperature, you can get a free API key at: https://openweathermap.org/api
  */
 
-public class CaliforniaWeather {
+public class CaliforniaWeather implements ActionListener {
 	HashMap<String, WeatherData> weatherData;
     void start() {
     	weatherData = Utilities.getWeatherData();
         
+    	
+    	
+    	button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		
         // All city keys have the first letter capitalized of each word
         String cityName = Utilities.capitalizeWords( "National City" );
         WeatherData datum = weatherData.get(cityName);
@@ -45,18 +62,61 @@ public class CaliforniaWeather {
         }
     }
         
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      ////////////////////////////////////////     GIU STUFF      ///////////////////////////////////////////////////////////
+    
+    JFrame frame = new JFrame();
+    JButton button1 = new JButton("Find weather of city");
+    JButton button2 = new JButton("Find cities with common weather");
+    JButton button3 = new JButton("Find cities with common temperature");
+    JPanel panel = new JPanel();
+    
+    
+    
+    
+    	////////////////////////////////     HASHMAP STUFF     ////////////////////////////////////////////////////////
         
     	public void step1() {
         	String city = JOptionPane.showInputDialog("Please enter your city: ");
             WeatherData charlie = weatherData.get(city); 
-            System.out.println("The weather looks like " + charlie.weatherSummary + " and the temperature is " + charlie.temperatureF+"F.");
+            JOptionPane.showMessageDialog(null,"The weather looks like " + charlie.weatherSummary + " and the temperature is " + charlie.temperatureF+"F.");
         }
     	public void step2() {
-    		
+    		String result = "";
+    		String weather = JOptionPane.showInputDialog("Please enter your type of weather: ");
+    		for(String city : weatherData.keySet()) {
+    			if(weatherData.get(city).weatherSummary.equals(weather)) {
+    				result += city + ", ";
+    			}
+    		}
+    		JOptionPane.showMessageDialog(null,result);
     	}
+    	public void step3() {
+    		String result = "";
+    		String lowerLim = JOptionPane.showInputDialog("Please enter your lower temperature limit: ");
+    		String upperLim = JOptionPane.showInputDialog("Please enter your upper temperature limit: ");
+    		int lowerLimI = Integer.parseInt(lowerLim);
+    		int upperLimI = Integer.parseInt(upperLim);
+    		for(String city : weatherData.keySet()) {
+    			if(weatherData.get(city).temperatureF > lowerLimI && weatherData.get(city).temperatureF < upperLimI) {
+    				result += city + ", ";
+    			}
+    		}
+    		JOptionPane.showMessageDialog(null,result);
+    	}
+		@Override
+		public void actionPerformed(ActionEvent blah) {
+			if(blah.getSource() == button1) {
+				step1();
+			}
+			if(blah.getSource() == button2) {
+				step2();
+			}
+			if(blah.getSource() == button3) {
+				step3();
+			}
+		}
     	
-    	// Create a frame and a button and call functions above when button is pressed. Have a gui to get California's weather conditions. 
+    	
         
         
         
